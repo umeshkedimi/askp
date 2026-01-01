77463796e12ca5b7a860df3362df5a283215ee75
umeshkedimi <umesh836@gmail.com>
2026-01-01 17:32:36 +0530

    feat(common): add Redis client bootstrap for cache + token revocation

---
+++ common/redis.py
@@ -0,0 +1,9 @@
+import redis
+from common.config import settings
+
+redis_client = redis.Redis(
+    host=settings.REDIS_HOST,
+    port=settings.REDIS_PORT,
+    db=settings.REDIS_DB,
+    decode_responses=True
+)
